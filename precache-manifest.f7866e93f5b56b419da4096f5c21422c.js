fe942ff0632fc7d0214c8621cee5e33ed6693be9
Alvin Yang <alvin.yang73@gmail.com>
2020-02-28 17:13:04 -0800

    Updates

---
+++ precache-manifest.f7866e93f5b56b419da4096f5c21422c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc06174ac83c7313e2c5907010dd5618",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "e864fb12ba8e843ed3e4",
+    "url": "/memory-game/static/css/main.9fadd978.chunk.css"
+  },
+  {
+    "revision": "c4978dc36e2b47514da7",
+    "url": "/memory-game/static/js/2.c9799693.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/memory-game/static/js/2.c9799693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e864fb12ba8e843ed3e4",
+    "url": "/memory-game/static/js/main.824e4546.chunk.js"
+  },
+  {
+    "revision": "7ba82378826073dce3ea",
+    "url": "/memory-game/static/js/runtime-main.02752f75.js"
+  }
+]);
